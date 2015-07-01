5bf25f6ef40f4df8660931760088b303df9ea379
Trung Nguyen <trungnguyenn269@gmail.com>
2015-07-01 20:57:25 +0700

    Correct src value of iframe

--- webui/src/main/webapp/scripts/app/home/home.jsx
+++ webui/src/main/webapp/scripts/app/home/home.jsx
@@ -4,7 +4,7 @@ var Home = React.createClass({
             <Container cls="jumbotron api-frame">
                 <Container cls="container">
                     <Container cls="embed-responsive embed-responsive-16by9">
-                        <iframe className="embed-responsive-item" src="api/index.html">Loading...</iframe>
+                        <iframe className="embed-responsive-item" src="api/index.html?url=/api/api-docs">Loading...</iframe>
                     </Container>
                 </Container>
             </Container>
