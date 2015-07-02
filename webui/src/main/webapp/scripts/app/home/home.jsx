d6414596909ad3b7c77732086d26865eb0cbc1e9
huytrongnguyen <huytrongnguyen@kms-technology.com>
2015-07-02 12:36:58 +0700

    authen, login, logout

--- webui/src/main/webapp/scripts/app/home/home.jsx
+++ webui/src/main/webapp/scripts/app/home/home.jsx
@@ -1,18 +1,7 @@
 var Home = React.createClass({
     render: function() {
         return <ContainerFluid>
-            <Container cls="jumbotron">
-                <Container cls="container">
-                    <Container cls="embed-responsive embed-responsive-16by9">
-                        <p>
-                        The worldwide most beautiful homepage will be loaded here soon.
-                        </p>
-                         <p>
-                         Contact <a href="https://github.com/huytrongnguyen/mstore-mono">huytrongnguyen</a> for more information.
-                         </p>
-                    </Container>
-                </Container>
-            </Container>
+            <Container cls="center-block slide-title" />
         </ContainerFluid>;
     }
 });
